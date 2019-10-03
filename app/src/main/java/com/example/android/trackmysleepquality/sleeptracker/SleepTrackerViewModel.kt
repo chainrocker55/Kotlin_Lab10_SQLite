ae9b0fa0f3eb34decfd36e067a351b62ea077c64
chainrocker55 <chainrock55@gmail.com>
2019-10-03 17:30:05 +0700

    Finish 6.2 OnClear Stop Start

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -61,6 +61,7 @@ class SleepTrackerViewModel(
         uiScope.launch {
             val newNight = SleepNight()
             insert(newNight)
+            tonight.value = getTonightFromDatabase()
         }
     }
 
@@ -70,10 +71,37 @@ class SleepTrackerViewModel(
         }
 
     }
+    fun onStopTracking(){
+        uiScope.launch {
+            val oldNight = tonight.value?: return@launch
+            oldNight.endTimeMilli = System.currentTimeMillis()
+            update(oldNight)
+        }
+    }
+    private suspend fun update(night: SleepNight) {
+        withContext(Dispatchers.IO){
+            database.update(night)
+        }
+
+    }
+    fun onClear(){
+        uiScope.launch {
+            clear()
+            tonight.value = null
+        }
+    }
+
+    private suspend fun clear() {
+        withContext(Dispatchers.IO){
+            database.clear()
+        }
+
+    }
 
     override fun onCleared() {
         super.onCleared()
         viewModelJob.cancel()
     }
+
 }
 
