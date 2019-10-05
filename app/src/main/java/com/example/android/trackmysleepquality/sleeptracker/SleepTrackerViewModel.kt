30254ea64f1f068fd12d2894317b5af316faa6dc
chainrocker55 <chainrock55@gmail.com>
2019-10-05 22:18:48 +0700

    Add visible button

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -41,6 +41,17 @@ class SleepTrackerViewModel(
     private var tonight = MutableLiveData<SleepNight?>()
     private val _navigateToSleepQuality = MutableLiveData<SleepNight>()
     val navigateToSleepQuality: LiveData<SleepNight> get() = _navigateToSleepQuality
+
+    val startButtonVisible = Transformations.map(tonight){
+        it == null
+    }
+    val stopButtonVisible = Transformations.map(tonight){
+        it != null
+    }
+    val clearButtonVisible = Transformations.map(nights){
+        it.isNotEmpty()
+    }
+
     init {
         initialixeTonight()
     }
