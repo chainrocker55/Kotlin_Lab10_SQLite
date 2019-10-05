01a5aacf05cc5f1ea3a49161f64092a8e2596fd1
chainrocker55 <chainrock55@gmail.com>
2019-10-05 22:25:56 +0700

    Snackbar

--- app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
+++ app/src/main/java/com/example/android/trackmysleepquality/sleeptracker/SleepTrackerViewModel.kt
@@ -52,6 +52,13 @@ class SleepTrackerViewModel(
         it.isNotEmpty()
     }
 
+    private var _showSnackbarEvent = MutableLiveData<Boolean>()
+    val showSnackbarEvent : LiveData<Boolean> get() = _showSnackbarEvent
+
+    fun doneShowingSnackbar(){
+        _showSnackbarEvent.value = null
+    }
+
     init {
         initialixeTonight()
     }
@@ -103,6 +110,7 @@ class SleepTrackerViewModel(
         uiScope.launch {
             clear()
             tonight.value = null
+            _showSnackbarEvent.value = true
         }
     }
 
